39ce5b498f831598d2b01468f287140ad246415d
wusel42 <wusel+src@uu.org>
2017-04-09 13:29:05 +0200

    Test

--- site.mk
+++ site.mk
@@ -14,7 +14,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
-	gluon-config-mode-geo-location-ffgt \
+	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
